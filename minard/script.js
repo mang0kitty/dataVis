a9a34be8005356bc0acb58589b320859adf228a2
Aideen Fay <me@aideen.dev>
2020-02-17 11:35:30 +0000

    feat: add temperature graph

--- minard/script.js
+++ minard/script.js
@@ -1,6 +1,5 @@
 function drawLine(data, canvas) {
   const widthScale = 0.0000025;
-
   const linesLayer = canvas.append("svg:g");
 
   linesLayer
@@ -47,53 +46,71 @@ function drawCityText(data, canvas) {
 
   return citiesLayer;
 }
-function drawMovementCanvas() {
+function drawCanvas() {
   return d3
     .select("body")
     .append("svg:svg")
-    .attr("viewBox", "22 -58 16 6")
+    .attr("viewBox", "22 -58 17 12")
     .attr("class", "movementCanvas");
 }
-
-function drawTemperatureCanvas() {
-  const canvas = d3
-    .select("body")
-    .append("svg:svg")
-    .attr("viewBox", [0, -40, 30, 20])
-    .attr("class", "tempCanvas");
-
-  return canvas;
-}
 function drawTemperature(data, canvas) {
-  const temperatureLayer = canvas.append("svg:g");
-  //set the ranges
-  const timeScale = d3
-    .scaleTime()
-    .domain([new Date(1812, 12, 5), new Date(1813, 11, 1)])
-    .range([0, 700]);
-  const tenpScale = d3
+  temperatureLayer = canvas.append("svg:g");
+  scale = d3
     .scaleLinear()
-    .domain([-40, 10])
-    .range(0, 40);
+    .domain([-30, 0])
+    .range([-48, -50]);
 
-  const valuesLine = d3
-    .line()
-    .x(d => d.day)
-    .y(d => d.temp)
-    .attr("class", "temp-line");
+  const line = temperatureLayer
+    .selectAll("line")
+    .data(data)
+    .enter()
+    .append("svg:line")
+    .attr("x1", d => d.lng)
+    .attr("y1", d => scale(d.temp))
+    .attr("x2", (d, i) => (data[i + 1] || d).lng)
+    .attr("y2", (d, i) => scale((data[i + 1] || d).temp))
+    .attr("class", "temperature-line");
+
+  const text = temperatureLayer
+    .selectAll("text.temp-text")
+    .data(data)
+    .enter()
+    .append("svg:text")
+    .attr("class", "temp-text")
+    .attr("x", d => d.lng)
+    .attr("y", d => scale(d.temp))
+    .text(d => `${d.temp}`);
+
+  return temperatureLayer;
+}
+function connectYLines(data, canvas) {
+  const connectYLinesLayer = canvas.append("svg:g");
+  scale = d3
+    .scaleLinear()
+    .domain([-30, 0])
+    .range([-48, -50]);
+  const line = connectYLinesLayer
+    .selectAll("line")
+    .data(data)
+    .enter()
+    .append("svg:line")
+    .attr("x1", d => d.temperatures.lng)
+    .attr("y1", d => scale(d.temperatures.temp))
+    .attr("x2", d => d.temperatures.lng)
+    .attr("y2", d => d.movements.lat)
+    .attr("class", "connectYLine");
+  return connectYLinesLayer;
 }
 
 function renderGraph(data) {
-  const movementCanvas = drawMovementCanvas();
-  const temperatureCanvas = drawTemperatureCanvas();
-  const troopLinesLayer = drawLine(data.movements, movementCanvas);
+  const canvas = drawCanvas();
+
+  const troopLinesLayer = drawLine(data.movements, canvas);
   troopLinesLayer.selectAll("line.advance-line").raise();
-  const cityText = drawCityText(data.cities, movementCanvas);
-  const survivorsLayer = drawSurvivorCount(data.movements, movementCanvas);
-  const temperatureLayer = drawTemperature(
-    data.temperatures,
-    temperatureCanvas
-  );
+  const cityText = drawCityText(data.cities, canvas);
+  const survivorsLayer = drawSurvivorCount(data.movements, canvas);
+  const temperatureLayer = drawTemperature(data.temperatures, canvas);
+  const connectYLinesLayer = connectYLines(data, canvas);
 }
 function loadData() {
   return fetch("data.json").then(response => response.json());
